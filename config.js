7e5a022113f4d1fe857f3bc0cfbe771acac9b09a
example <dev@example.com>
2024-11-02 11:36:19 +0800

    修改地址

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const apiUrl = 'https://airplay-tv.lixiang4u.xyz'
 const websocketAddr = 'wss://airplay-tv.lixiang4u.xyz/api/wss'
-const joinRoomUrl = 'https://airplay-tv.lixiang4u.xyz/#/group/join'
+const joinRoomUrl = 'https://airplay-tv.pages.dev/#/group/join'
 const libmediaAvpUrl = 'https://libmedia-avp.pages.dev/'
 const defaultCover = 'https://iph.href.lu/360x528?text=.&fg=bcbcbc&bg=eeeeee'
 
